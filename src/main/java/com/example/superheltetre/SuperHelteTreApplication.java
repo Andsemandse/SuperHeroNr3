d37e05b516a6cbf824586320634da85975e158fd
Andsemandse <andr90m3@stud.kea.dk>
2023-02-20 18:19:34 +0100

    .

---
+++ src/main/java/com/example/superheltetre/SuperHelteTreApplication.java
@@ -0,0 +1,13 @@
+package com.example.superheltetre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperHelteTreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperHelteTreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/superheltetre/SuperHelteTreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superheltetre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHelteTreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
